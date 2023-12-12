c528e0c5c618ec30a9f040dce1016983c7986044
cadekellar <cadekellar@gmail.com>
2023-12-11 19:26:19 -0600

    Add project files.

---
+++ Controllers/SongsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Assignment5.Data;
+using Assignment5.Models;
+
+namespace Assignment5.Controllers
+{
+    public class SongsController : Controller
+    {
+        private readonly Assignment5Context _context;
+
+        public SongsController(Assignment5Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Songs
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Songs.ToListAsync());
+        }
+
+        // GET: Songs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var songs = await _context.Songs
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (songs == null)
+            {
+                return NotFound();
+            }
+
+            return View(songs);
+        }
+
+        // GET: Songs/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Songs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre")] Songs songs)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(songs);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(songs);
+        }
+
+        // GET: Songs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var songs = await _context.Songs.FindAsync(id);
+            if (songs == null)
+            {
+                return NotFound();
+            }
+            return View(songs);
+        }
+
+        // POST: Songs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre")] Songs songs)
+        {
+            if (id != songs.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(songs);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SongsExists(songs.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(songs);
+        }
+
+        // GET: Songs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var songs = await _context.Songs
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (songs == null)
+            {
+                return NotFound();
+            }
+
+            return View(songs);
+        }
+
+        // POST: Songs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var songs = await _context.Songs.FindAsync(id);
+            if (songs != null)
+            {
+                _context.Songs.Remove(songs);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SongsExists(int id)
+        {
+            return _context.Songs.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Models/Song.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment5.Models
+{
+    public class Songs
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+
+
+
+    }
+}
