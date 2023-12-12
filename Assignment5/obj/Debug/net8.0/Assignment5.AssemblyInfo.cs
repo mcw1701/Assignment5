05037e6c9970e03ef747914a16ae953256024ffb
mcw1701 <maeson.williams@ndus.edu>
2023-12-12 03:29:54 -0600

    fix

--- Assignment5/obj/Debug/net8.0/Assignment5.AssemblyInfo.cs
+++ Assignment5/obj/Debug/net8.0/Assignment5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2b387549bc5518a85e2bea4259365784ddd107b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
